4338fe4c2c07fb476cc172d3f5142a304eb18a5f
PUPPALAEswar <112838722+PUPPALAEswar@users.noreply.github.com>
2025-12-17 12:07:13 +0530

    Create ReplaceElementsWithGreatestOnRight.java

---
+++ Arrays/ReplaceElementsWithGreatestOnRight.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] replaceElements(int[] arr) {
+        int ans=-1;
+        for(int i=arr.length-1;i>=0;i--){
+                int cur=arr[i];
+                arr[i]=ans;
+                ans=Math.max(ans,cur);
+            }
+        return arr;
+        
+    }
+}
